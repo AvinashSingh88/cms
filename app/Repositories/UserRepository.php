a27ea221bb2119626c3b09ce87287fc88b370f51
Avinash Singh <104144851+AvinashSingh88@users.noreply.github.com>
2023-04-08 17:28:06 +0530

    updated user list

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -9,6 +9,7 @@ class UserRepository implements UserRepositoryInterface
     public function allUsers(){
         $users = User::select('users.*', 'user_types.name AS user_type_name')
             ->leftJoin('user_types', 'users.user_type_id', '=', 'user_types.id')
+            ->where('users.user_type_id', '!=', 1)
             ->latest()->paginate(10);
         return $users;
     }
